c2e2ff7a1be09d2f523103963826ec0ce145718b
Savkov, Andrey <andred9991@gmail.com>
2023-08-10 19:28:40 +0300

    Add Tests

---
+++ Telegram_AI_Bot.Tests/TelegramMessageHelperTests.cs
@@ -0,0 +1,40 @@
+using OpenAI.Chat;
+using Telegram_AI_Bot.Core.Models;
+using Telegram_AI_Bot.Core.Telegram;
+using Role = OpenAI.Chat.Role;
+
+namespace Telegram_AI_Bot.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GetNumTokensFromMessages_WhenGPT4()
+    {
+        var messages = new Message[]
+        {
+            new(Role.System, "System Message"),
+            new(Role.User, "Hello"),
+            new(Role.Assistant, "Hello2"),
+        };
+        var result = TelegramMessageHelper.GetNumTokensFromMessages(ChatModel.Gpt4, messages);
+        Assert.That(result, Is.EqualTo(20));
+    }
+    
+    [Test]
+    public void GetNumTokensFromMessages_WhenGPT35()
+    {
+        var messages = new Message[]
+        {
+            new(Role.System, "System Message"),
+            new(Role.User, "Hello"),
+            new(Role.Assistant, "Hello2"),
+        };
+        var result = TelegramMessageHelper.GetNumTokensFromMessages(ChatModel.Gpt35, messages);
+        Assert.That(result, Is.EqualTo(23));
+    }
+}
