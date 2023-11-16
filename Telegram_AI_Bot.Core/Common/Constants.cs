24ca019985e34fb42917a89090ff502c17304e02
Savkov, Andrey <andred9991@gmail.com>
2023-11-17 00:36:03 +0300

    Increase the Free Start Balance

--- Telegram_AI_Bot.Core/Common/Constants.cs
+++ Telegram_AI_Bot.Core/Common/Constants.cs
@@ -3,7 +3,7 @@ namespace Telegram_AI_Bot.Core.Common;
 public class Constants
 {
     public const int MAX_STORED_MESSAGES = 20;
-    public const int FREE_START_BALANCE = 10000;
+    public const int FREE_START_BALANCE = 20000;
     public const int LOW_LIMIT_BALANCE = 5000;
     public const int GPT4_FACTOR = 15;
     public const int LIFETIME_OF_TYPING = 360; // seconds
